d99805cfeb08b4aa1fa69d5ea4a9b89bb6f0b962
danielparedeshuaracha <danielparedeshuaracha@gmail.com>
2022-07-04 23:25:34 -0500

    firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,12 +4,12 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJLln-pqjNcSRzh4_ybKktiL1DNLd0F-o",
-  authDomain: "ecommerce-e4e6c.firebaseapp.com",
-  projectId: "ecommerce-e4e6c",
-  storageBucket: "ecommerce-e4e6c.appspot.com",
-  messagingSenderId: "1040611516041",
-  appId: "1:1040611516041:web:21b3ff13346db595d87058",
+  apiKey: "AIzaSyDnBE_fgeiz1GnmW7YxHq5sI2YD_MlYgoc",
+  authDomain: "ecommerce-c0adb.firebaseapp.com",
+  projectId: "ecommerce-c0adb",
+  storageBucket: "ecommerce-c0adb.appspot.com",
+  messagingSenderId: "710419082453",
+  appId: "1:710419082453:web:5e0d47d396b0cdb129465c",
 };
 
 const firebaseApp = !firebase.apps.length
